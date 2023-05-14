d661a8741813a2e311e0dd994fceb061fbd480fe
henryda2004 <henryda2004@gmail.com>
2023-05-13 18:47:21 -0600

    4 mapas listos

--- cmake-build-debug/Proyecto_2_Datos_ll_autogen/EWIEGA46WW/moc_Ghost.cpp
+++ cmake-build-debug/Proyecto_2_Datos_ll_autogen/EWIEGA46WW/moc_Ghost.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'Ghost.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.6)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -13,7 +13,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'Ghost.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.15.6. It"
+#error "This file was generated using the moc from 5.15.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -83,7 +83,7 @@ void Ghost::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
         auto *_t = static_cast<Ghost *>(_o);
-        (void)_t;
+        Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->move(); break;
         case 1: _t->move2(); break;
